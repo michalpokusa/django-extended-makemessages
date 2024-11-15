efb4afe6b0f437f714e8a33cbbf142d02549d383
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2024-11-15 15:44:08 +0000

    Added --extract-all, --force-po and --indent arguments

--- django_extended_makemessages/management/commands/extendedmakemessages.py
+++ django_extended_makemessages/management/commands/extendedmakemessages.py
@@ -13,6 +13,17 @@ class Command(MakeMessagesCommand):
     @override
     def add_arguments(self, parser: CommandParser):
         super().add_arguments(parser)
+        parser.add_argument(
+            "--extract-all",
+            action="store_true",
+            help="Extract all strings.",
+        )
+        parser.add_argument(
+            "--force-po",
+            action="store_true",
+            help="Always write an output file even if no message is defined.",
+        )
+
         sort_group = parser.add_mutually_exclusive_group()
         sort_group.add_argument(
             "--sort-output",
@@ -25,12 +36,28 @@ class Command(MakeMessagesCommand):
             help="Sort output by file location.",
         )
 
+        parser.add_argument(
+            "--indent",
+            action="store_true",
+            help="Write the .po file using indented style.",
+        )
+
     @override
     def handle(self, *args, **options):
+
+        if options["extract_all"]:
+            self.xgettext_options.append("--extract-all")
+        if options["force_po"]:
+            self.xgettext_options.append("--force-po")
 
         if options["sort_output"]:
             self.msgmerge_options.append("--sort-output")
         if options["sort_by_file"]:
             self.msgmerge_options.append("--sort-by-file")
+
+        if options["indent"]:
+            self.xgettext_options.append("--indent")
+            self.msgmerge_options.append("--indent")
+            self.msgattrib_options.append("--indent")
 
         super().handle(*args, **options)
