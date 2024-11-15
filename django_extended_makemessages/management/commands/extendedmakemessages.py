cefc868e9c51565c4e2254984f675eeac68be1a2
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2024-11-15 14:40:00 +0000

    Added --sort-output and --sort-by-file arguments

--- django_extended_makemessages/management/commands/extendedmakemessages.py
+++ django_extended_makemessages/management/commands/extendedmakemessages.py
@@ -13,7 +13,24 @@ class Command(MakeMessagesCommand):
     @override
     def add_arguments(self, parser: CommandParser):
         super().add_arguments(parser)
+        sort_group = parser.add_mutually_exclusive_group()
+        sort_group.add_argument(
+            "--sort-output",
+            action="store_true",
+            help="Generate sorted output.",
+        )
+        sort_group.add_argument(
+            "--sort-by-file",
+            action="store_true",
+            help="Sort output by file location.",
+        )
 
     @override
     def handle(self, *args, **options):
+
+        if options["sort_output"]:
+            self.msgmerge_options.append("--sort-output")
+        if options["sort_by_file"]:
+            self.msgmerge_options.append("--sort-by-file")
+
         super().handle(*args, **options)
