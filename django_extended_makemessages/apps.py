ce6fc173b61de20f4a23f393d8981a910b561987
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2024-11-16 01:36:37 +0000

    Fix: Typo in app config name

--- django_extended_makemessages/apps.py
+++ django_extended_makemessages/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoExtendedMakemessagesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_extended_makemeessages"
+    name = "django_extended_makemessages"
