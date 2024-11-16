f44218429fb24af0d9e87a520bad263e4578503c
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2024-11-16 03:14:11 +0000

    Reordered arguments to match man pages order

--- django_extended_makemessages/management/commands/extendedmakemessages.py
+++ django_extended_makemessages/management/commands/extendedmakemessages.py
@@ -40,7 +40,12 @@ class Command(MakeMessagesCommand):
             if isinstance(action, _VersionAction):
                 action.version = django_extended_makemessages.__version__
 
-        # Add new arguments
+        # Command specific options
+        parser.add_argument(
+            "--no-fuzzy-matching",
+            action="store_true",
+            help="Do not use fuzzy matching when an exact match is not found. This may speed up the operation considerably.",
+        )
         parser.add_argument(
             "--extract-all",
             action="store_true",
@@ -51,21 +56,12 @@ class Command(MakeMessagesCommand):
             action="append",
             help="Specify keywordspec as an additional keyword to be looked for. Without a keywordspec, the option means to not use default keywords.",
         )
+
+        # Common options
         parser.add_argument(
             "--force-po",
             action="store_true",
             help="Always write an output file even if no message is defined.",
-        )
-        sort_group = parser.add_mutually_exclusive_group()
-        sort_group.add_argument(
-            "--sort-output",
-            action="store_true",
-            help="Generate sorted output.",
-        )
-        sort_group.add_argument(
-            "--sort-by-file",
-            action="store_true",
-            help="Sort output by file location.",
         )
         parser.add_argument(
             "--indent",
@@ -78,39 +74,56 @@ class Command(MakeMessagesCommand):
             action="store",
             help="Set the output page width. Long strings in the output files will be split across multiple lines in order to ensure that each line’s width (= number of screen columns) is less or equal to the given number.",
         )
-        parser.add_argument(
-            "--no-fuzzy-matching",
+        sort_group = parser.add_mutually_exclusive_group()
+        sort_group.add_argument(
+            "--sort-output",
             action="store_true",
-            help="Do not use fuzzy matching when an exact match is not found. This may speed up the operation considerably.",
+            help="Generate sorted output.",
+        )
+        sort_group.add_argument(
+            "--sort-by-file",
+            action="store_true",
+            help="Sort output by file location.",
         )
 
     @override
     def handle(self, *args, **options):
 
+        # Command specific options
+        if options["no_fuzzy_matching"]:
+            self.msgmerge_options.append("--no-fuzzy-matching")
         if options["extract_all"]:
             self.xgettext_options.append("--extract-all")
         if options["keyword"]:
             self.xgettext_options += [
                 f"--keyword={keywordspec}" for keywordspec in options["keyword"]
             ]
-        if options["force_po"]:
-            self.xgettext_options.append("--force-po")
 
+        # Common options
+        if options["force_po"]:
+            self.msgmerge_options.append("--force-po")
+            self.msguniq_options.append("--force-po")
+            self.msgattrib_options.append("--force-po")
+            self.xgettext_options.append("--force-po")
+        if options["indent"]:
+            self.msgmerge_options.append("--indent")
+            self.msguniq_options.append("--indent")
+            self.msgattrib_options.append("--indent")
+            self.xgettext_options.append("--indent")
+        if options["width"]:
+            self.msgmerge_options.append(f"--width={options['width']}")
+            self.msguniq_options.append(f"--width={options['width']}")
+            self.msgattrib_options.append(f"--width={options['width']}")
+            self.xgettext_options.append(f"--width={options['width']}")
         if options["sort_output"]:
             self.msgmerge_options.append("--sort-output")
+            self.msguniq_options.append("--sort-output")
+            self.msgattrib_options.append("--sort-output")
+            self.xgettext_options.append("--sort-output")
         if options["sort_by_file"]:
             self.msgmerge_options.append("--sort-by-file")
-
-        if options["indent"]:
-            self.xgettext_options.append("--indent")
-            self.msgmerge_options.append("--indent")
-            self.msgattrib_options.append("--indent")
-        if options["width"]:
-            self.xgettext_options.append(f"--width={options['width']}")
-            self.msgmerge_options.append(f"--width={options['width']}")
-            self.msgattrib_options.append(f"--width={options['width']}")
-
-        if options["no_fuzzy_matching"]:
-            self.msgmerge_options.append("--no-fuzzy-matching")
+            self.msguniq_options.append("--sort-by-file")
+            self.msgattrib_options.append("--sort-by-file")
+            self.xgettext_options.append("--sort-by-file")
 
         super().handle(*args, **options)
