07a7e0603b6f80ad75bbbd15c821458fb1ffdf2c
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2026-08-26 02:27:22 +0200

    Removed `--sort-untranslated-last` option

--- django_extended_makemessages/management/commands/extendedmakemessages.py
+++ django_extended_makemessages/management/commands/extendedmakemessages.py
@@ -286,12 +286,6 @@ class Command(MakeMessagesCommand):
             action="store_true",
             help="Sort output by file location.",
         )
-        parser.add_argument(
-            "--sort-untranslated-last",
-            action="store_true",
-            help="Sort untranslated messages after translated messages for easier review and translation.",
-        )
-
         # Custom options
         parser.add_argument(
             "--detect-aliases",
@@ -565,18 +559,6 @@ class Command(MakeMessagesCommand):
                     remove_obsolete_prefix(entry_match.group("msgid"))
                 )
                 return (is_obsolete, msgid)
-
-            self._sort_entries_in_po_file(pofile, key_func)
-
-        if self.options["sort_untranslated_last"]:
-
-            def key_func(entry_match: re.Match):
-                is_header = entry_match.start() == 0
-                is_obsolete = entry_match.group("obsolete") is not None
-                has_untranslated_msgstr = entry_has_untranslated_msgstr(
-                    entry_match.group()
-                )
-                return (not is_header, is_obsolete, has_untranslated_msgstr)
 
             self._sort_entries_in_po_file(pofile, key_func)
 
